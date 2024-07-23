f3735e24ffeb0b554a68777242a645652c436964
Malik <malik28067@mail.ru>
2024-07-23 13:54:00 +0300

    feat: register added

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -1,15 +1,44 @@
 import { Form, Row, Space, Typography } from 'antd';
-import { Link } from 'react-router-dom';
-import { Button, Input, Layout, PasswordInput } from '../../components';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { useRegisterMutation } from '../../app/services/auth';
+import { Button, ErrorMessage, Input, Layout, PasswordInput } from '../../components';
 import { Paths } from '../../consts';
+import { selectUser } from '../../features/auth/authSlice';
+import { User } from '../../types';
+import { isErrorWithMessage } from '../../utils';
 import { CardStyled } from './styled';
 
+type RegisterData = Omit<User, 'id'> & { confirmPassword: string };
+
 export const RegisterPage = () => {
+  const navigate = useNavigate();
+  const user = useSelector(selectUser);
+  const [error, setError] = useState('');
+  const [registerUser] = useRegisterMutation();
+
+  const register = async (data: RegisterData) => {
+    try {
+      await registerUser(data).unwrap();
+
+      navigate('/');
+    } catch (error) {
+      const maybeError = isErrorWithMessage(error);
+
+      if (maybeError) {
+        setError(error.data.message);
+      } else {
+        setError('Неизвестная ошибка');
+      }
+    }
+  };
+
   return (
     <Layout>
       <Row align="middle" justify="center">
         <CardStyled title="Зарегистрируйтесь">
-          <Form onFinish={() => null}>
+          <Form onFinish={register}>
             <Input name="name" placeholder="Имя" />
             <Input type="email" name="email" placeholder="Email" />
             <PasswordInput name="password" placeholder="Пароль" />
@@ -22,6 +51,7 @@ export const RegisterPage = () => {
             <Typography.Text>
               Уже зарегистрированы? <Link to={Paths.login}>Войдите</Link>
             </Typography.Text>
+            <ErrorMessage message={error} />
           </Space>
         </CardStyled>
       </Row>
